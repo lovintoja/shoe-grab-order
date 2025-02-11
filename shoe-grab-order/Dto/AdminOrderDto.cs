e799c5d81fb1b0e6357e902c84b3f817395947ee
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-11 17:28:06 +0100

    xxx

--- shoe-grab-order/Dto/AdminOrderDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShoeGrabOrderManagement.Dto;
-
-public class AdminOrderDto
-{
-    public OrderDto Order { get; set; }
-    public int UserId { get; set; }
-}
--- shoe-grab-order/Dto/ChangeOrderStatusDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShoeGrabOrderManagement.Dto;
-
-public class ChangeOrderStatusDto
-{
-    public int OrderId { get; set; }
-    public string OrderStatus { get; set; } = string.Empty;
-}
