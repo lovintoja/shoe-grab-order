bd336af64ca3b6f0e91c73383f90b10db27120d8
Filip Dumanowski <filip.dumanowski@qiagen.com>
2025-02-16 21:43:02 +0100

    sample test

---
+++ shoe-grab-tests/OrderTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ShoeGrabOrderManagementTests;
+public class OrderTests
+{
+    [Fact]
+    public async void Test1()
+    {
+        await Task.Delay(35000);
+
+        Assert.True(true);
+    }
+}
